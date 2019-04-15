6c8a0c49bf3c5ebcda26c1b054429e39c8945bae
julianjohannesen <github email address>
2019-04-15 13:17:31 -0400

    updated package.json, small edits to TodoItem and ItemOptions

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import ItemOptions from './ItemOptions.js'
 
 export default class TodoItem extends Component {
-  render() {
-    return (
-      <li>
-        <p>{this.props.todo.text}</p>
-        <ItemOptions />
-      </li>
-    )
-  }
+    render() {
+        return (
+            <li>
+                <p>{this.props.todo.text}</p>
+                <ItemOptions />
+            </li>
+        )
+    }
 }
